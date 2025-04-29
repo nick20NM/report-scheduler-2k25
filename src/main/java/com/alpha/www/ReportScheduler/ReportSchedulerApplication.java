99d31edc6ddbfe5083eeb8176ac24aa49562361b
Nick <masihnishit@gmail.com>
2025-04-29 20:14:41 +0530

    initial commit

---
+++ src/main/java/com/alpha/www/ReportScheduler/ReportSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.www.ReportScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSchedulerApplication.class, args);
+	}
+
+}
